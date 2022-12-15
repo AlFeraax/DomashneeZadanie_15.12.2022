942e938219c6661b086e118186ea5109945dbbd5
AlFeraax <grigolin@mail.ru>
2022-12-16 02:24:31 +0300

    пятую задачу сделал

---
+++ Zadanie5/Program.cs
@@ -0,0 +1,7 @@
+﻿// Задание 5. Показать последнюю цифру трёхзначного числа.
+int number = new Random().Next(100, 999);
+int lastDigit = (number % 10);
+Console.Write("Дано число: ");
+Console.WriteLine(number);
+Console.Write("Его крайняя цифра равна: ");
+Console.WriteLine(lastDigit);
