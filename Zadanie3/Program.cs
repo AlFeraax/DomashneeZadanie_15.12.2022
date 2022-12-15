b31c748fcbf0c407ab308cf681039e93b21b2004
AlFeraax <grigolin@mail.ru>
2022-12-16 01:07:55 +0300

    третью задачу сделал

---
+++ Zadanie3/Program.cs
@@ -0,0 +1,19 @@
+﻿int a = new Random().Next(0, 50);
+int b = new Random().Next(0, 50);
+int c = new Random().Next(0, 50);
+int max = a;
+if (b > a)
+    max = b;
+if (c > b)
+    max = c;
+if (c > a)
+    max = c;
+
+Console.Write("Дано число ");
+Console.WriteLine(a);
+Console.Write("Дано число ");
+Console.WriteLine(b);
+Console.Write("Дано число ");
+Console.WriteLine(c);
+Console.Write(max);
+Console.WriteLine(" это наибольшее число");
