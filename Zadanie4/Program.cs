8a13562561e734c574e3493368d9690d0e01a7e2
AlFeraax <grigolin@mail.ru>
2022-12-16 02:02:03 +0300

    четвертую задачу сделал

---
+++ Zadanie4/Program.cs
@@ -0,0 +1,11 @@
+﻿// задача 4. Выяснить является ли число чётным
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 2 == 0)
+{
+    Console.WriteLine("Это число четное.");
+}
+else if (number % 2 != 0)
+{
+    Console.WriteLine("Это число не четное.");
+}
