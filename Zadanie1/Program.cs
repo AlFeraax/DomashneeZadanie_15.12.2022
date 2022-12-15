ca1592594cbd8e92e15f0a35b3459292d800824f
AlFeraax <grigolin@mail.ru>
2022-12-15 23:59:28 +0300

    первую задачу сделал

---
+++ Zadanie1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 1. По двум заданным числам проверять является ли первое квадратом второго
+int a = new Random().Next(1, 10);
+int b = new Random().Next(1, 10);
+if (b * b != a)
+{
+    Console.Write(a);
+    Console.WriteLine(" не является квадратом ");
+    Console.Write(b);
+}
+else if (b * b == a)
+{
+    Console.Write(a);
+    Console.WriteLine(" является квадратом ");
+    Console.Write(b);
+}
