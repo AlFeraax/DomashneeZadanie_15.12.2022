b5717742005335e4ac9a5fe08ccd43d4756af736
AlFeraax <grigolin@mail.ru>
2022-12-16 00:44:05 +0300

    вторую задачу сделал

---
+++ Zadanie2/Program.cs
@@ -0,0 +1,31 @@
+﻿//По заданному номеру дня недели вывести его название
+Console.WriteLine("Введите номер дня недели: ");
+int daynumber = Convert.ToInt32(Console.ReadLine());
+if (daynumber == 1)
+{
+    Console.WriteLine("это Понедельник");
+}
+if (daynumber == 2)
+{
+    Console.WriteLine("это Вторник");
+}
+if (daynumber == 3)
+{
+    Console.WriteLine("это Среда");
+}
+if (daynumber == 4)
+{
+    Console.WriteLine("это Четверг");
+}
+if (daynumber == 5)
+{
+    Console.WriteLine("это Пятница");
+}
+if (daynumber == 6)
+{
+    Console.WriteLine("это Суббота");
+}
+if (daynumber == 7)
+{
+    Console.WriteLine("это Воскресенье");
+}
