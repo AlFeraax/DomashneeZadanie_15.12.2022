2070fadee419740ff80a45ae8b6fe2b1e601106a
AlFeraax <grigolin@mail.ru>
2022-12-16 03:00:39 +0300

    шестую задачу сделал

---
+++ Zadanie6/Program.cs
@@ -0,0 +1,11 @@
+﻿//Задача 6. Показать вторую цифру трёхзначного числа.
+int number = new Random().Next(100, 999);
+int lastTwoDigits = (number % 100);
+int middleDigit = (lastTwoDigits / 10);
+if (lastTwoDigits <= 99)
+{
+    Console.Write("Дано число: ");
+    Console.WriteLine(number);
+    Console.Write("Его среднеяя цифра это: ");
+    Console.WriteLine(middleDigit);
+}
