f3308c4e1c8db54241ed4fc96cbbb1eb44246d31
AlFeraax <grigolin@mail.ru>
2022-12-16 12:38:20 +0300

    девятую задачу сделал

---
+++ Zadanie9/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задание 9. Выяснить, кратно ли число заданному, если нет, вывести остаток.
+double number = new Random().Next(2, 9);
+Console.WriteLine("Введите двузначное число: ");
+int entNumber = Convert.ToInt32(Console.ReadLine());
+if (entNumber % number == 0)
+{
+    Console.Write("Дано число: ");
+    Console.WriteLine(number);
+    Console.Write("Введено число: ");
+    Console.WriteLine(entNumber);
+    Console.WriteLine("Введенное число кратно данному");
+}
+else if (entNumber % number != 0)
+{
+    Console.Write("Дано число: ");
+    Console.WriteLine(number);
+    Console.Write("Введено число: ");
+    Console.WriteLine(entNumber);
+    Console.Write("Введенное число не кратно данному, остаток от деления составляет: ");
+    Console.WriteLine(entNumber / number);
+}
